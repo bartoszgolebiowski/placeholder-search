39ceae3f770a0039ea90a6c23385104e9aaf00d0
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2021-11-21 22:21:08 +0100

    improvment rename page catalog to pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@theme-ui/core";
 import * as React from "react";
 import { Provider } from "react-redux";
 
-import UsersSearch from "./page/Users";
+import UsersSearch from "./pages/Users";
 import { store } from "./store/store";
 import { theme } from "./theme";
 
